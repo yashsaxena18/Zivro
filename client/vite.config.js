c8f85fa0e64edcd330ae11c4ce4d36afe6becff5
Yash Saxena <yashkishu505@gmail.com>
2026-02-04 19:46:04 +0530

    Updating Vite config js file

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      // Ensure Rollup/Vite resolves the distributable for production builds on Vercel
+      "socket.io-client": "socket.io-client/dist/socket.io.js",
+    },
+  },
   plugins: [react()],
   server: {
     host: "localhost", // dev only
